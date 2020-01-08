3eebebd941ac561b4a81dab9ef1393268d76c840
yashpatel5566789 <yashpatel5566789@gmail.com>
2020-01-08 23:56:41 +0530

    fixed error

--- restapi/serializers.py
+++ restapi/serializers.py
@@ -22,36 +22,37 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ResumeSerializer(serializers.ModelSerializer):
-    users = UserSerializer(many=True)
+   # users_set = UserSerializer(many=True)
     class Meta:
         model = Resume
         fields = '__all__'
+
 class WorkExperienceSerializer(serializers.ModelSerializer):
-    resumes = ResumeSerializer(many=True)
+    #resumes_set = ResumeSerializer(many=True)
     class Meta:
         model = WorkExperience
         fields = '__all__'
 
 class CertificationSerializer(serializers.ModelSerializer):
-    resumes = ResumeSerializer(many=True)
+    #resumes_set = ResumeSerializer(many=True)
     class Meta:
         model = Certification
         fields = '__all__'
 
 class EducationSerializer(serializers.ModelSerializer):
-    resumes = ResumeSerializer(many=True)
+    #resumes_set = ResumeSerializer(many=True)
     class Meta:
         model = Education
         fields = '__all__'
 
 class SkillSerializer(serializers.ModelSerializer):
-    resumes = ResumeSerializer(many=True)
+    #resumes_set = ResumeSerializer(many=True)
     class Meta:
         model = Skill
         fields = '__all__'
 
 class LanguageSerializer(serializers.ModelSerializer):
-    resumes = ResumeSerializer(many=True)
+   # resumes_set = ResumeSerializer(many=True)
     class Meta:
         model = Language
         fields = '__all__'
